b17fbcd6574cc241da6e436537ace61b44bd1259
Bradley Meardon <bradley.meardon@gmail.com>
2021-09-21 22:40:25 +0100

    Experimenting with Document commands
    Basic HTML document with a script to test and experiment DOM

---
+++ script.js
@@ -0,0 +1,5 @@
+// calls back all the classes in the document called "second"
+document.getElementsByClassName("second");
+
+// calls back the ID in the document called "first"
+document.getElementById("first");
