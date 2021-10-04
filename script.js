7d0aba59b6903fc1ebfdc88395ff4c346e2352c6
Bradley Meardon <bradley.meardon@gmail.com>
2021-10-04 19:33:56 +0100

    Progressing on the tutorials
    Using more functions with DOM

--- script.js
+++ script.js
@@ -3,3 +3,62 @@ console.log(document.getElementsByClassName("second"));
 
 // calls back the ID in the document called "first"
 console.log(document.getElementById("first"));
+
+// calls back the h1 tag
+
+console.log(document.querySelector("h1"));
+
+// calls back first li
+
+console.log(document.querySelector("li"));
+
+// calls back all li elements
+
+console.log(document.querySelectorAll("li"));
+
+
+// get the first li elements attribute
+
+var firstLi = document.querySelector("li");
+
+console.log(firstLi.getAttribute("random"));
+
+// change the attribute
+
+firstLi.setAttribute("random","1000");
+
+console.log(firstLi);
+
+// Changing styles on elements
+
+// document.querySelector("h1").style.background = "yellow";
+
+// changing element style using class in stylesheet
+
+var h1 = document.querySelector("h1");
+
+h1.className = "coolTitle";
+
+// display element style on li
+
+document.querySelector("li").classList;
+
+// toggling a class to the list of li elements
+
+document.querySelector("li").classList.toggle("done");
+
+// using innerHTML to change the text
+
+document.querySelector("h1").innerHTML = "<strong>!!!!</strong>";
+
+// select second element in the list
+
+console.log(document.querySelectorAll("li")[1]);
+
+// select the parent element of the li
+
+console.log(document.querySelectorAll("li")[1].parentElement);
+
+// select the children of the body
+
+console.log(document.querySelectorAll("li")[1].parentElement.parentElement.children);
